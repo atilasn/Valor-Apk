c4db5771edafae55bd30b2232275a3fb9c09b854
Atila <atilasn90@gmail.com>
2020-11-16 20:24:04 -0300

    pronto!

--- lib/Pages/Resultado.dart
+++ lib/Pages/Resultado.dart
@@ -40,7 +40,7 @@ class _Resultado extends State<Resultado> {
                       right: 150,
                     ),
                     child: Text(
-                      'Valor = ',
+                      'Valori = ',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
